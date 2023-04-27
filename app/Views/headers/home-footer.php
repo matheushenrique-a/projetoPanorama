7a70cd42f3648b5b13a87b827a0c3646ab688518
dantas <fernando.dantas@gmail.com>
2023-04-27 10:52:29 -0300

    ajustes

--- app/Views/headers/home-footer.php
+++ app/Views/headers/home-footer.php
@@ -733,15 +733,19 @@
 								
 						
 						?>
-						<div class="d-flex justify-content-start mb-10">
+						<div class="d-flex justify-content-start mb-3">
 							<div class="d-flex flex-column align-items-start">
 								<div class="d-flex align-items-center mb-2">
-									<div class="symbol symbol-35px symbol-circle">
+									<div class="symbol symbol-30px symbol-circle">
 										<img alt="Pic" src="<?php echo assetfolder ?>assets/pravoce/media/logos/pravc_chat.png" />
 									</div>
 									<div class="ms-3">
+										<?php 
+											$dataEv = date("d/M H:i:s", strtotime($row->last_update . ' +2 hours'));
+										
+										?>
 										<a href="#" class="fs-5 fw-bold text-gray-900 text-hover-primary me-1">PRAVOCE</a>
-										<span class="text-muted fs-7 mb-1"><?php echo time_elapsed_string($row->last_update);?></span>
+										<span class="text-muted fs-7 mb-1"><?php echo $dataEv . " - " . time_elapsed_string($row->last_update);?></span>
 									</div>
 								</div>
 								<div class="p-5 rounded bg-light-info text-dark fw-semibold mw-lg-400px text-start" data-kt-element="message-text"><?php echo translate_telegram($row->categoria) . " " . telegram_message_adjust($row->categoria, $row->mensagem);?></div>
@@ -758,30 +762,6 @@
 					<!--end::Messages-->
 				</div>
 				<!--end::Card body-->
-				<!--begin::Card footer-->
-				<div class="card-footer pt-4" id="kt_drawer_chat_messenger_footer">
-					<!--begin::Input-->
-					<textarea class="form-control form-control-flush mb-3" rows="1" data-kt-element="input" placeholder="Type a message"></textarea>
-					<!--end::Input-->
-					<!--begin:Toolbar-->
-					<div class="d-flex flex-stack">
-						<!--begin::Actions-->
-						<div class="d-flex align-items-center me-2">
-							<button class="btn btn-sm btn-icon btn-active-light-primary me-1" type="button" data-bs-toggle="tooltip" title="Coming soon">
-								<i class="bi bi-paperclip fs-3"></i>
-							</button>
-							<button class="btn btn-sm btn-icon btn-active-light-primary me-1" type="button" data-bs-toggle="tooltip" title="Coming soon">
-								<i class="bi bi-upload fs-3"></i>
-							</button>
-						</div>
-						<!--end::Actions-->
-						<!--begin::Send-->
-						<button class="btn btn-primary" type="button" data-kt-element="send">Send</button>
-						<!--end::Send-->
-					</div>
-					<!--end::Toolbar-->
-				</div>
-				<!--end::Card footer-->
 			</div>
 			<!--end::Messenger-->
 		</div>
