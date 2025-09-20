362c6e7d239769f7dc15d7a79819df4e5d7f190d
whitwander <vcmikhaelcp@gmail.com>
2025-09-20 09:15:07 -0300

    ip

--- app/Filters/IpFilter.php
+++ app/Filters/IpFilter.php
@@ -8,11 +8,13 @@ use CodeIgniter\Filters\FilterInterface;
 
 class IpFilter implements FilterInterface
 {
-    private $allowed_ips = ['127.0.0.1', '::1', '177.73.197.2', '201.77.167.249'];
+    private $allowed_ips = ['127.0.0.1', '::1', '177.73.197.2', '2804:33cc:1434:d400:28fc:6cf6:678:7816'];
 
     public function before(RequestInterface $request, $arguments = null)
     {
         $ip = $request->getIPAddress();
+
+        return print_r($ip);
 
         if (!in_array($ip, $this->allowed_ips)) {
             return service('response')
